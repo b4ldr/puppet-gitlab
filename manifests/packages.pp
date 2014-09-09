b433e64bbebaa8cb3b53b54413230f5eb966cfec
b4ldr <github@johnbond.org>
2014-09-09 10:30:27 +0100

    dont need to specify gems package

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -45,7 +45,6 @@ class gitlab::packages inherits gitlab {
 
   class { 'ruby':
     ruby_package      =>  'ruby1.9.1-full',
-    rubygems_package  =>  'rubygems1.9.1',
     rubygems_update   =>  true,
     gems_version      =>  'latest',
   }
